8a6cd8bed38734681f1687b3b9932ec788ad167f
Alissa <alissa.work17@aol.com>
2024-12-02 16:19:44 -0500

    add untracked files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector("#letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for(letter of word){
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function(e){
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+})
+
